03544990ac5a505585f1d0df1dc1e22e97a61e5c
Gabriel Dias <83024986+BielDias2501@users.noreply.github.com>
2025-02-21 15:01:28 -0300

    Adicionei o get pelo ID

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/servicos', (requisicao,resposta) => {
         }
         resposta.status(200).json(bancoDados);        
     } catch (error) {
-        resposta.status(500).json({mensagem:"Erro ao buscar produto",
+        resposta.status(500).json({mensagem:"Erro ao buscar servico",
             erro: error.message
         })
     }
@@ -28,11 +28,11 @@ app.get('/servicos', (requisicao,resposta) => {
 //CRIAR SERVIÇO
 app.post('/servicos', (requisicao,resposta) => {
     try {
-        const {id, Cliente, Endereco, TipoServico, DataHora, Status} = requisicao.body
-        if(!id || !Cliente || !Endereco || !TipoServico || !DataHora || !Status){
+        const {id, cliente, endereco, tiposervico, datahora, status} = requisicao.body
+        if(!id || !cliente || !endereco || !tiposervico || !datahora || !status){
             return resposta.status(200).json({mensagem:"Todos os dados devem ser inseridos"})
         }
-        const novoServico = {id, Cliente, Endereco, TipoServico, DataHora, Status};
+        const novoServico = {id, cliente, endereco, tiposervico, datahora, status};
         bancoDados.push(novoServico)
         resposta.status(201).json({mensagem:"Servico criado com sucesso"});
     } catch (error) {
@@ -45,7 +45,7 @@ app.put('/servicos/:id', (requisicao, resposta) => {
     //http:localhost:3000/servicos/1
     try {
         const id = requisicao.params.id;
-        const {novoCliente, novoEndereco, novoTipoServico, novaDataHora, novoStatus } = requisicao.body
+        const {novoCliente, novoEndereco, novoTiposervico, novaDatahora, novoStatus } = requisicao.body
         if(!id){
             return resposta.status(404).json({mensagem:"informe um paramentro"})
         }
@@ -53,16 +53,32 @@ app.put('/servicos/:id', (requisicao, resposta) => {
         if(!servico){
             return resposta.status(404).json({mensagem:"Servico não encontrado"})
         }
-        servico.Cliente = novoCliente || servico.Cliente
-        servico.Endereco = novoEndereco || servico.Endereco
-        servico.TipoServico = novoTipoServico || servico.TipoServico
-        servico.DataHora = novaDataHora || servico.DataHora
-        servico.Status = novoStatus || servico.Status
+        servico.cliente = novoCliente || servico.cliente
+        servico.endereco = novoEndereco || servico.endereco
+        servico.tiposervico = novoTiposervico || servico.tiposervico
+        servico.datahora = novaDatahora || servico.datahora
+        servico.status = novoStatus || servico.status
         resposta.status(200).json({mensagem:"Servico atualizado com sucesso"})
     } catch (error) {
         resposta.status(500).json({mensagem:"Erro ao editar servicos", erro: error.message})
     }
 })
+
+//BUSCAR PELO ID
+app.get('/servicos/:id', (requisicao,resposta) => {
+    try {
+      const id = requisicao.params.id
+      const servico = bancoDados.find(elemento => elemento.id === id)
+      if(!servico){
+        return resposta.status(404).json({mensagem:"servico não encontrado"})
+      }
+      resposta.status(200).json(servico)
+    } catch (error) {
+      resposta.status(500).json({mensagem:"Erro ao buscar o servico", erro: error.message})
+    }
+  });
+  
+
 
 //DELETAR SERVICOS
 app.delete('/servicos/:id', (requisicao,resposta) => {
